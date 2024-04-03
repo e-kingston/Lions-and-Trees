f16844cafab12f431f5f8caebd9436abf85b45d4
Eden <kingstoneden@gmail.com>
2024-04-03 08:40:29 -0700

    add about page content

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,3 +1,4 @@
+import About from "../components/about";
 import Introduction from "../components/introduction";
 import { Navigation } from "../components/navigation";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navigation />
       <Introduction />
+      <About />
     </>
   )
 }
