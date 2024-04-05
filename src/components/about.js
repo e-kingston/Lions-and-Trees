ec126b2070baf0909769edc8fa7644c872c039cb
Alfonce Nzioka <alfonce.starfish@gmail.com>
2024-04-04 20:22:17 -0700

    add some styling to about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,29 +1,36 @@
 export default function About() {
   return (
-    <div className="section">
-      <h2>
-        About Us
-      </h2>
-      <p> 
-      Lions & Trees is a family-run enterprise that provides high-touch personal financial advice 
-      to its clients at industry-low costs. Named after the currency in Narnia*, mentioned during 
-      Prince Caspian's epic adventure to the edge of the Eastern Ocean, our company endeavors to 
-      multiply our clients' "Lions" & "Trees" -- that is, improve their financial health. We do 
-      this by analyzing everything from small to big -- from the day-to-day decision-making to 
-      the long-term stewardship of assets -- as we guide clients in the direction of their specific
-     financial objectives.
+    <div className="section container">
+      <h2 className="text-center section-title">About Us</h2>
+      <blockquote className="blockquote text-right">
+        <p cla="mb-3">
+          Their search revealed, one by one, a helmet, a dagger, and a few
+          coins...genuine Narnian 'Lions' and 'Trees' such as you might see any
+          day in the marketplace of Beaversdam or Beruna.
+        </p>
+        <footer className="blockquote-footer">
+          <cite>The Voyage of the
+        Dawn Treader, The Chronicles of Narnia</cite> by C.S Lewis
+        </footer>
+      </blockquote>
+      <p>
+        Lions & Trees is a family-run enterprise that provides high-touch
+        personal financial advice to its clients at industry-low costs. Named
+        after the currency in Narnia*, mentioned during Prince Caspian's epic
+        adventure to the edge of the Eastern Ocean, our company endeavors to
+        multiply our clients' "Lions" & "Trees" -- that is, improve their
+        financial health. We do this by analyzing everything from small to big
+        -- from the day-to-day decision-making to the long-term stewardship of
+        assets -- as we guide clients in the direction of their specific
+        financial objectives.
       </p>
       <p>
-      Lions & Trees features the family team of dad Pete and twin teenagers John and Eden. Pete has
-       worked in finance for more than a decade and has developed a deep reservoir of personal 
-       finance expertise and experience. John and Eden handle the business's back end, including
-        brand management, web development, bookkeeping, and even some research.
-      </p>
-      <p>
-      *"Their search revealed, one by one, a helmet, a dagger, and a few coins...genuine Narnian
-       'Lions' and 'Trees' such as you might see any day in the marketplace of Beaversdam or Beruna."
-        - The Voyage of the Dawn Treader, The Chronicles of Narnia, by C.S. Lewis
+        Lions & Trees features the family team of dad Pete and twin teenagers
+        John and Eden. Pete has worked in finance for more than a decade and has
+        developed a deep reservoir of personal finance expertise and experience.
+        John and Eden handle the business's back end, including brand
+        management, web development, bookkeeping, and even some research.
       </p>
     </div>
-  )
+  );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import About from "../components/about";
 import Introduction from "../components/introduction";
 import { Navigation } from "../components/navigation";
+import SectionSeparator from "../components/section-separator";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navigation />
       <Introduction />
       <About />
+      <SectionSeparator />
     </>
-  )
+  );
 }
