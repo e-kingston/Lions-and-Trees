8875f60bdfe67295a4a4fc0acf56961ac50ad6ed
Eden <kingstoneden@gmail.com>
2024-04-06 10:34:35 -0700

    client page first steps

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import About from "../components/about";
 import Introduction from "../components/introduction";
 import SectionSeparator from "../components/section-separator";
 import WhyUs from "../components/why-us";
+import Clients from "../components/clients";
 
 export default function Home() {
   return (
@@ -10,6 +11,8 @@ export default function Home() {
       <About />
       <SectionSeparator />
       <WhyUs />
+      <SectionSeparator />
+      <Clients />
     </>
   );
 }
